9e995b42c2b524ee3989ddc613310e8e4cfbd188
Eddington Rowellis <eddingtonibi@gmail.com>
2023-10-15 16:04:07 +0300

    string,list,comma to list

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
-def oxford_comma(items):
-    return None
+# def oxford_comma(items):
+#     return None
+def oxford_comma(array):
+    if len(array) == 1:
+        return array[0]
+    elif len(array) == 2:
+        return " and ".join(array)
+    else:
+        last_element = array.pop()
+        return ", ".join(array) + ", and " + last_element
